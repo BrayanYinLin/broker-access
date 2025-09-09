52ba67d4840aca3f2bffc7fb5dadd6b397d5e5d2
Brayan Yin Lin <brayanyl250206@gmail.com>
2025-09-09 15:50:14 -0500

    feat: client dependencies frozen by security and proxy created

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+	plugins: [react()],
+	server: {
+		proxy: {
+			'/api': {
+				target: 'http://localhost:3000/api/v1',
+				changeOrigin: true,
+				secure: false,
+				rewrite: (path) => path.replace(/^\/api/, '')
+			}
+		}
+	}
 })
